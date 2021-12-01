6c252223ad5ed1768731d6b81774ea6d7415334c
Vicente Reig Rincon de Arellano <vicente@tray.io>
2021-12-01 15:13:24 +0100

    day 01: first part

--- lib/advent_of_code/measurements.ex
+++ lib/advent_of_code/measurements.ex
@@ -4,21 +4,38 @@ defmodule AdventOfCode.Measurements do
   There is no measurement before the first measurement.
 
   ## Examples
-
+    Empty Sonar Report
     iex> AdventOfCode.Measurements.count([])
     0
-  """
-  def count(measurements) when length(measurements) < 2, do: 0
 
-  def count(measurements) when length(measurements) > 1 do
+    A Sonar Report with a single measurement
+    iex> AdventOfCode.Measurements.count([100])
+    0
+
+    A Sonar Report over a flat sea floort.
+    iex> AdventOfCode.Measurements.count([1000,1000,1000,1000,1000])
+    0
+
+    A Sonar Report over a Valley
+    iex> AdventOfCode.Measurements.count([1000,1100,1200,1500,1200,1100,1000])
+    3
+
+    A Rocky Sea Floor Report (Taken from the problem statement: https://adventofcode.com/2021/day/1)
+    iex> AdventOfCode.Measurements.count([199, 200, 208, 210,200,207,240,269,260, 263])
+    7
+  """
+  def count(measurements) when length(measurements) == 0, do: 0
+
+  def count(measurements) when length(measurements) > 0 do
     previous_measurements = [0] ++ Enum.slice(measurements, 0, Enum.count(measurements) - 1)
     deltas = Enum.zip_with([measurements, previous_measurements], fn [current, prev] -> cond do
+      prev == 0 -> 0
       current > prev -> 1
       current < prev -> -1
       true -> 0
       end
     end)
 
-    Enum.count(deltas, fn delta -> delta > 0 end)
+    Enum.count(deltas, fn delta -> delta == 1 end)
   end
 end
--- test/advent_of_code/measurements_test.exs
+++ test/advent_of_code/measurements_test.exs
@@ -2,22 +2,11 @@ defmodule MeasurementsTest do
   use ExUnit.Case
   doctest AdventOfCode.Measurements
 
-  test "empty sea floor depth report" do
-    assert AdventOfCode.Measurements.count([]) == 0
-  end
-
-  test "flat sea floor report" do
-    assert AdventOfCode.Measurements.count([1000,1000,1000,1000,1000]) == 1
-  end
-
-  test "rocky see floor report" do
-    assert AdventOfCode.Measurements.count([1000,2000,1000,2000,1000]) == 3
-  end
-
   test 'day01 run' do
     measurements = File.read!("data/day01/input.txt")
-                   |> String.split("\n", trim: true)
-                   |> Enum.map(&Integer.parse/1)
-    assert AdventOfCode.Measurements.count(measurements) == 1549
+                   |> String.trim()
+                   |> String.split("\n")
+                   |> Enum.map(&String.to_integer/1)
+    assert AdventOfCode.Measurements.count(measurements) == 1548
   end
 end
