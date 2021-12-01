e8755f53f2a71aa8124d03b782c0ac91539d0451
Vicente Reig Rincon de Arellano <vicente@tray.io>
2021-12-01 14:28:36 +0100

    Counts depth increases

--- lib/advent_of_code/measurements.ex
+++ lib/advent_of_code/measurements.ex
@@ -8,7 +8,17 @@ defmodule AdventOfCode.Measurements do
     iex> AdventOfCode.Measurements.count([])
     0
   """
-  def count(measurements) do
-    Enum.count(measurements)
+  def count(measurements) when length(measurements) < 2, do: 0
+
+  def count(measurements) when length(measurements) > 1 do
+    previous_measurements = [0] ++ Enum.slice(measurements, 0, Enum.count(measurements) - 1)
+    deltas = Enum.zip_with([measurements, previous_measurements], fn [current, prev] -> cond do
+      current > prev -> 1
+      current < prev -> -1
+      true -> 0
+      end
+    end)
+
+    Enum.count(deltas, fn delta -> delta > 0 end)
   end
 end
--- test/advent_of_code/measurements_test.exs
+++ test/advent_of_code/measurements_test.exs
@@ -2,7 +2,15 @@ defmodule MeasurementsTest do
   use ExUnit.Case
   doctest AdventOfCode.Measurements
 
-  test "basic case" do
+  test "empty sea floor depth report" do
     assert AdventOfCode.Measurements.count([]) == 0
+  end
+
+  test "flat sea floor report" do
+    assert AdventOfCode.Measurements.count([1000,1000,1000,1000,1000]) == 1
+  end
+
+  test "rocky see floor report" do
+    assert AdventOfCode.Measurements.count([1000,2000,1000,2000,1000]) == 3
   end
 end
