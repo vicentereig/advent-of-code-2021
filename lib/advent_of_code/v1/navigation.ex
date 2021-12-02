7e040a43632418f468c2bdcd98f039c05a8a5177
Vicente Reig Rincon de Arellano <vicente@tray.io>
2021-12-02 17:13:13 +0100

    simplified v1

--- lib/advent_of_code/v1/navigation.ex
+++ lib/advent_of_code/v1/navigation.ex
@@ -24,19 +24,12 @@ defmodule AdventOfCode.V1.Navigation do
     %AdventOfCode.V1.Submarine{x: 5, y: 5}
   """
   def play(commands) do
-    forward_commands =
-      Enum.filter(commands, fn {:ok, {command, _position}} -> command == :forward end)
-
-    forward_deltas = Enum.map(forward_commands, fn {:ok, {:forward, position}} -> position end)
-    x = Enum.sum(forward_deltas)
-
-    up_commands = Enum.filter(commands, fn {:ok, {command, _position}} -> command == :up end)
-    up_deltas = Enum.map(up_commands, fn {:ok, {:up, position}} -> position end)
-
-    down_commands = Enum.filter(commands, fn {:ok, {command, _position}} -> command == :down end)
-    down_deltas = Enum.map(down_commands, fn {:ok, {:down, position}} -> position end)
-    y = Enum.sum(down_deltas) - Enum.sum(up_deltas)
-
-    %AdventOfCode.V1.Submarine{x: x, y: y}
+    Enum.reduce(commands, %AdventOfCode.V1.Submarine{}, fn command, submarine ->
+      case command do
+        {:ok, {:forward, position}} -> Map.put(submarine, :x, submarine.x + position)
+        {:ok, {:down, position}} -> Map.put(submarine, :y, submarine.y + position)
+        {:ok, {:up, position}} -> Map.put(submarine, :y, submarine.y - position)
+      end
+    end)
   end
 end
