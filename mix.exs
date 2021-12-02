57a9b809612694a263f591264fa07db4dc75eb91
Vicente Reig Rincon de Arellano <vicente@tray.io>
2021-12-02 16:51:09 +0100

    add dialyzed

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule AdventOfCode.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyzex, "~> 1.2.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
