6ca3000aed3e02fed5b0a01e2236899fdbc34a8e
Vicente Reig Rincon de Arellano <vicente@tray.io>
2021-12-03 16:09:42 +0100

    diagnostics

--- lib/advent_of_code/diagnostics.ex
+++ lib/advent_of_code/diagnostics.ex
@@ -1,5 +1,132 @@
 defmodule AdventOfCode.Diagnostics do
+
+  def calculate_co2_scrubber_rating(report) do
+    rating = find_co2_scrubber_rating(report)
+    String.to_integer(rating, 2)
+  end
+
+  def find_co2_scrubber_rating(report) do
+    counts = count_bits(report)
+
+    initial_state = %{
+      numbers: report,
+      most_common_bit:
+        if(Enum.at(counts, 0).ones_count >= Enum.at(counts, 0).zeros_count,
+          do: "0",
+          else: "1"
+        ),
+      index: 0,
+      rate: nil
+    }
+
+    last_iteration = length(report) * String.length(Enum.at(report, 0)) - 1
+    iterations = 0..last_iteration
+
+    Enum.reduce_while(iterations, initial_state, fn _iter, state ->
+      cond do
+        length(state.numbers) == 1 ->
+          {:halt, Enum.at(state.numbers, 0)}
+
+        true ->
+          new_selected_numbers =
+            Enum.filter(state.numbers, fn number ->
+              String.at(number, state.index) == state.most_common_bit
+            end)
+
+          counts = count_bits(new_selected_numbers)
+          cond do
+            state.index >= length(counts) ->
+              IO.inspect(state)
+              {:cont, %{ state | result: String.to_integer(Enum.at(new_selected_numbers, 0), 2)}}
+
+            state.index < length(counts) ->
+              next_index = state.index + 1
+              {:cont,
+                %{
+                  numbers: new_selected_numbers,
+                  most_common_bit:
+                    if(
+                      next_index < length(counts) && Enum.at(counts, next_index).ones_count >=
+                        Enum.at(counts, next_index).zeros_count,
+                      do: "0",
+                      else: "1"
+                    ),
+                  index: state.index + 1,
+                  result: nil
+                }}
+          end
+      end
+    end)
+  end
+
+  def calculate_oxygen_generator_rating(report) do
+    rating = find_oxygen_generator_rating(report)
+    String.to_integer(rating, 2)
+  end
+
+
+  def find_oxygen_generator_rating(report) do
+    counts = count_bits(report)
+
+    initial_state = %{
+      numbers: report,
+      most_common_bit:
+        if(Enum.at(counts, 0).ones_count >= Enum.at(counts, 0).zeros_count,
+          do: "1",
+          else: "0"
+        ),
+      index: 0,
+      rate: nil
+    }
+
+    last_iteration = length(report) * String.length(Enum.at(report, 0)) - 1
+    iterations = 0..last_iteration
+
+    Enum.reduce_while(iterations, initial_state, fn _iter, state ->
+      cond do
+        length(state.numbers) == 1 ->
+          {:halt, Enum.at(state.numbers, 0)}
+
+        true ->
+          new_selected_numbers =
+            Enum.filter(state.numbers, fn number ->
+              String.at(number, state.index) == state.most_common_bit
+            end)
+
+          counts = count_bits(new_selected_numbers)
+          cond do
+            state.index >= length(counts) ->
+            IO.inspect(state)
+              {:cont, %{ state | result: String.to_integer(Enum.at(new_selected_numbers, 0), 2)}}
+
+            state.index < length(counts) ->
+              next_index = state.index + 1
+              {:cont,
+               %{
+                 numbers: new_selected_numbers,
+                 most_common_bit:
+                   if(
+                     next_index < length(counts) && Enum.at(counts, next_index).ones_count >=
+                       Enum.at(counts, next_index).zeros_count,
+                     do: "1",
+                     else: "0"
+                   ),
+                 index: state.index + 1,
+                 result: nil
+               }}
+          end
+      end
+    end)
+  end
+
   def calculate_power_consumption(report) do
+    counts = count_bits(report)
+    gamma_rate = String.to_integer(calculate_gamma_rate(counts), 2)
+    epsilon_rate = String.to_integer(calculate_epsilon_rate(counts), 2)
+    gamma_rate * epsilon_rate
+  end
+
+  def count_bits(report) do
     matrix = Enum.map(report, &String.graphemes/1)
     initial_counts = %{ones_count: 0, zeros_count: 0}
 
@@ -12,10 +139,6 @@ defmodule AdventOfCode.Diagnostics do
           end
         end)
       end)
-
-    gamma_rate = String.to_integer(calculate_gamma_rate(counts),2)
-    epsilon_rate = String.to_integer(calculate_epsilon_rate(counts), 2)
-    gamma_rate * epsilon_rate
   end
 
   def calculate_gamma_rate(counts) do
@@ -37,7 +160,6 @@ defmodule AdventOfCode.Diagnostics do
     end)
     |> Enum.join()
   end
-
 
   def transpose([]), do: []
   def transpose([[] | _]), do: []
--- test/advent_of_code/diagnostics_test.exs
+++ test/advent_of_code/diagnostics_test.exs
@@ -1,11 +1,13 @@
 defmodule AdventOfCode.DiagnosticsTest do
   use ExUnit.Case
+
   test 'day03 part 1' do
     report =
       File.read!("data/day03/input.txt")
       |> String.trim()
       |> String.split("\n")
-    assert 3309596 == AdventOfCode.Diagnostics.calculate_power_consumption(report)
+
+    assert 3_309_596 == AdventOfCode.Diagnostics.calculate_power_consumption(report)
   end
 
   test 'calculates the power consumption' do
@@ -25,5 +27,43 @@ defmodule AdventOfCode.DiagnosticsTest do
     ]
 
     assert 198 == AdventOfCode.Diagnostics.calculate_power_consumption(report)
+  end
+
+  test 'calculates oxygen generator rating' do
+    report = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010"
+    ]
+
+    assert 23 == AdventOfCode.Diagnostics.calculate_oxygen_generator_rating(report)
+  end
+
+  test 'calculates co2 scrubber rating' do
+    report = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010"
+    ]
+
+    assert 10 == AdventOfCode.Diagnostics.calculate_co2_scrubber_rating(report)
   end
 end
