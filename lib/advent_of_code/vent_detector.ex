531c781811b1e452e7bb5eb5894be9dff411491f
Vicente Reig Rincon de Arellano <vicente@tray.io>
2021-12-06 14:13:16 +0100

    counts intersections

--- lib/advent_of_code/vent_detector.ex
+++ lib/advent_of_code/vent_detector.ex
@@ -37,22 +37,22 @@ defmodule AdventOfCode.VentDetector do
 
     iex> segments = [AdventOfCode.Segment.create([0,0], [0,5])]
     ...> AdventOfCode.VentDetector.create_canvas(segments)
-    [[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]]
+    [[1,1,1,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
-    iex> segments = [AdventOfCode.Segment.create([0,0], [0,5]), AdventOfCode.Segment.create([0,0], [3,0])]
+    iex> segments = [AdventOfCode.Segment.create([0,0], [0,5]), AdventOfCode.Segment.create([0,0], [0, 2])]
     ...> segments
     ...> |> AdventOfCode.VentDetector.create_canvas
-    [[2,1,1,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]]
+    [[2,2,2,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
   """
 
   def create_canvas(segments) do
     [south_east | _] =
       Enum.flat_map(segments, fn s -> [s.start, s.end] end)
       |> Enum.uniq()
-      |> Enum.sort(fn a, b -> a.y > b.y and a.x > b.x end)
+      |> Enum.sort(fn a, b -> a.y >= b.y and a.x >= b.x end)
 
     Enum.map(0..(south_east.y - 1), fn x ->
-      Enum.map(0..(south_east.x + south_east.y - 1), fn y ->
+      Enum.map(0..((south_east.x + south_east.y)-1), fn y ->
         intersection_count(segments, x, y)
       end)
     end)
